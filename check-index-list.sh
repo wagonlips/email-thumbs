6cd35de750192281a1eb50122a72a24e6e92376d
webmaster-peet <scamden@peets.com>
2015-01-28 11:47:28 -0800

    Commented out stat file times, will come back to that.

--- check-index-list.sh
+++ check-index-list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 THUMBSDIR=`pwd -P`
-find $THUMBSDIR/ -name \*.html | sort -r | sed '1d' > /tmp/current-html.list
+find $THUMBSDIR/ -name \*.html | sort -r | sed '/index.html/d' > /tmp/current-html.list
 
 # check for changes in directory structure
 if diff /tmp/current-html.list $THUMBSDIR/index_files/email-html.list >/dev/null
@@ -17,6 +17,7 @@ if diff /tmp/current-html.list $THUMBSDIR/index_files/email-html.list >/dev/null
 fi
 
 # record the modification times of each file
-while read line
-  do echo $(stat -c %Y-%n $line) >> $THUMBSDIR/file-times.list
-done < $THUMBSDIR/index_files/email-html.list
+# while read line
+#   do echo $(stat -c %Y-%n $line) >> $THUMBSDIR/file-times.list
+# done < $THUMBSDIR/index_files/email-html.list
+# for automation
