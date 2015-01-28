a1db025c6fcfa5c6ff647b1ebecd0538d91f7d17
webmaster-peet <scamden@peets.com>
2015-01-28 14:38:23 -0800

    Started but did not finish for loop. Still need to get image URL into the mix

--- make-index.sh
+++ make-index.sh
@@ -4,7 +4,6 @@
 #
 file_url="dev-misc.peets.com/email-thumbs/EMAIL_2015/1st_qtr/150201-esn-vday/150201-esn-vday.html"
 thumb_url="dev-misc.peets.com_email-thumbs_EMAIL_2015_1st_qtr_150201-esn-vday_150201-esn-vday.html.png"
-caption=$(basename $file_url)
 cat > index.html <<INDEX_HEAD
 <!DOCTYPE html>
 <html>
@@ -15,14 +14,23 @@ cat > index.html <<INDEX_HEAD
 </style>
 </head>
 <body>
-<div class="year-container" id="thumbs-2015">
-  <div class="thumb">
-    <div class="caption">$caption</div>
-    <a href="http://$file_url"><img src="images/$thumb_url" width="250" alt="" /></a>
-  </div>
-</div>
-</body>
-</html>
 INDEX_HEAD
 
+for i in index_files/2015.list index_files/2014.list index_files/2013.list index_files/2012.list index_files/2011.list
+  do
+    echo "<div class=\"year-container\" id=\"t-`basename $i`\">"
+    while read line
+      do 
+        caption=$(basename $line)
+        echo "  <div class=\"thumb\">"
+        echo "    <div class=\"caption\">$caption</div>"
+        echo "    <a href=\"http://$line\"><img src=\"images/$thumb_url\" width=\"250\" alt=\"\" /></a>"
+        echo "  </div>"
+      done < $i
+    echo "</div>"
+  done
+cat >> index.html <<INDEX_FOOT
+</body>
+</html>
+INDEX_FOOT
 
