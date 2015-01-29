2c6cd10e2f962d049820f02eb188d89cffd14c2a
webmaster-peet <scamden@peets.com>
2015-01-28 16:21:46 -0800

    It worked. But it creates an index file that is browser-crushingly large. Will have to break it up.

--- make-index.sh
+++ make-index.sh
@@ -2,15 +2,15 @@
 #
 # this is script 3 of 3
 #
-file_url="dev-misc.peets.com/email-thumbs/EMAIL_2015/1st_qtr/150201-esn-vday/150201-esn-vday.html"
-thumb_url="dev-misc.peets.com_email-thumbs_EMAIL_2015_1st_qtr_150201-esn-vday_150201-esn-vday.html.png"
 cat > index.html <<INDEX_HEAD
 <!DOCTYPE html>
 <html>
 <head>
 <title>Peet&rsquo;s Coffee &amp; Tea E-Commerce Marketing Emails</title>
 <style type="text/css">
+.year-container {border:1px solid green;}
 .thumb {float:left;text-align:center;}
+.hidden {display:none;}
 </style>
 </head>
 <body>
@@ -18,16 +18,21 @@ INDEX_HEAD
 
 for i in index_files/2015.list index_files/2014.list index_files/2013.list index_files/2012.list index_files/2011.list
   do
-    echo "<div class=\"year-container\" id=\"t-`basename $i`\">"
+    echo "<div class=\"year-container\" id=\"t-`basename $i`\"> `basename $i`" >> index.html
+    echo "<div class=\"hidden\">" >> index.html
     while read line
       do 
         caption=$(basename $line)
-        echo "  <div class=\"thumb\">"
-        echo "    <div class=\"caption\">$caption</div>"
-        echo "    <a href=\"http://$line\"><img src=\"images/$thumb_url\" width=\"250\" alt=\"\" /></a>"
-        echo "  </div>"
+        thumb_url="$(echo $line | tr "/" "_").png"
+cat >> index.html <<INDEX_BODY
+<div class="thumb">
+  <div class="caption">$caption</div>
+  <a href="http://$line"><img src="images/$thumb_url" width="250" alt="" /></a>
+</div>
+INDEX_BODY
       done < $i
-    echo "</div>"
+    echo "</div>" >> index.html
+    echo "</div>" >> index.html
   done
 cat >> index.html <<INDEX_FOOT
 </body>
