fabc0c14918568947a44ac5d16a6a0f4695960f2
webmaster-peet <scamden@peets.com>
2015-03-05 14:23:47 -0800

    Thumbs generated as PNGs and then converted to JPGs. Seems to be working.

--- make-thumbs.sh
+++ make-thumbs.sh
@@ -7,11 +7,13 @@ THUMBSDIR=`pwd -P`
 # first delete the old file
 rm $THUMBSDIR/index_files/thumbs-url.list
 # then make a new one
-while read line
 # the file "email-html.list" must be generated first by the "check-index-list.sh" script
 # we're using THUMBSDIR to mask the actual directory location and to aid in portability
 # the sed instruction below 's|.{40}|...' simply swaps the first 40 characters from each
 # line with the domain and will need to be changed if the HTML files' location changes
+
+# thumbs-url.list will be used to create the main index file.
+while read line
   do echo $line | sed -r 's|.{40}|dev-misc.peets.com|' >> $THUMBSDIR/index_files/thumbs-url.list
 done < $THUMBSDIR/index_files/email-html.list
 
@@ -27,25 +29,29 @@ awk -F, '/EMAIL_2011/' $THUMBSDIR/index_files/thumbs-url.list > $THUMBSDIR/index
 echo "mini files done!"
 
 # then run the thumbs command on the mini lists
-for i in $THUMBSDIR/index_files/2015.list $THUMBSDIR/index_files/2014.list $THUMBSDIR/index_files/2013.list $THUMBSDIR/index_files/2012.list $THUMBSDIR/index_files/2011.list
-  do
-    echo "Rendering thumbs for" $i
+# for i in $THUMBSDIR/index_files/2015.list $THUMBSDIR/index_files/2014.list $THUMBSDIR/index_files/2013.list $THUMBSDIR/index_files/2012.list $THUMBSDIR/index_files/2011.list
+#while read LINE1
+#  do
 # Only make thumbs for those HTML files modified within the last 30 days.
-    while read LINE
+    while read SERVERURLS
       do
-        if test ! `find $LINE -mtime +30` 
+        if test ! `find $SERVERURLS -mtime +30` 
         then
-          phantomjs $THUMBSDIR/render_multi_url_email_thumbs.js $LINE
+          echo "Rendering thumbs for" $SERVERURLS
+          WEBURLS=$(sed -r 's|.{40}|dev-misc.peets.com|' <<< $SERVERURLS)
+          phantomjs $THUMBSDIR/render_multi_url_email_thumbs.js $WEBURLS
         fi
-      done < $i
-  done
+      done < $THUMBSDIR/index_files/email-html.list
+#      done
 
 echo "PNGs made"
 
-exit 0
-
 for i in $THUMBSDIR/images/*.png
-  do convert $i -resize 250 $i.jpg
+  do
+    if test ! `find $i -mtime +1` 
+      then
+        convert $i -resize 250 $i.jpg
+    fi
   done
 
 echo "JPGs made"
